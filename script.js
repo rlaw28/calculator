083a795a8589c252eef46ad9ceeca0bb2baa819f
rlaw28 <rashaunlawson28@gmail.com>
2023-01-15 20:07:40 -0500

    Add operate function to evaluate numbers with different math operators.

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const mathBtns = document.querySelectorAll('button');
 mathBtns.forEach(button => button.addEventListener('click', () => {
    result.textContent = button.textContent;
  }));
+
+const operate = function ( num1, operator, num2) {
+  if (operator === '+') {
+    return num1 + num2
+  }
+  else if (operator === '-') {
+    return num1 - num2
+  }
+  else if (operator === '*') {
+    return num1 * num2
+  }
+  else if (operator === '/') {
+    return num1 / num2
+  }
+  else {
+    return 'Error'
+  }
+};
+
+console.log(operate(3, '+', 3));
