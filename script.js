c55c5042e204d493dc8e034a86d6679658a0ec08
rlaw28 <rashaunlawson28@gmail.com>
2023-01-18 05:04:16 -0500

    Update foreach button method.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const result = document.querySelector('.result');
 const mathBtns = document.querySelectorAll('button');
 
-let total = 0
-  
+let runningTotal = 0
+result.innerText = 0
 
 mathBtns.forEach(button => button.addEventListener('click', () => {
-  result.textContent = button.textContent;
- }));
+  let currentNum = button.textContent
+  result.innerText += currentNum;
+  }));
 
 const operate = function ( num1, operator, num2) {
   if (operator === '+') {
