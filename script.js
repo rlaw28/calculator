02a63299091964183901caf02e02c29704781338
rlaw28 <rashaunlawson28@gmail.com>
2023-01-27 08:45:52 -0500

    Refactor forEach methods to select from number and operator buttons and add functions to handlethem respectively.

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const currentNum = document.querySelector('.current');
+const currentScreen = document.querySelector('.current');
+const previousScreen = document.querySelector('.previous');
 const equal = document.querySelector('.equal');
-const number = document.querySelectorAll('.number');
-const operator = documnet.querySelectorAll('.operator');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
 
-current.innerText = ''
+let currentValue = '';
+let previousValue = '';
 
-// mathBtns.forEach(button => button.addEventListener('click', () => {
-//   let currentNum = button.textContent
-//   result.innerText += currentNum;
-// }));
-  
-// calculate.addEventListener('click', () => {
-//  operate();
-// })
+numbers.forEach((number) => number.addEventListener('click', function(e){
+  handleNumber(e.target.textContent);
+  currentScreen.textContent = currentValue;
+}))
+
+operators.forEach((op) => op.addEventListener('click', function (e) {
+  handleOpertator(e.target.textContent);
+  previousScreen.textContent = previousValue + " " + operator;
+  currentScreen.textContent = currentValue;
+}))
+
+function handleNumber(num) {
+  if (currentValue.length <= 20) {
+    currentValue += num;
+  }
+}
+function handleOpertator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = '';
+}
 
 const operate = function ( num1, operator, num2) {
   if (operator === '+') {
