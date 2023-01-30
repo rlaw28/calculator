3c65058c1627e16f52446db56df11c8b8b34a85d
rlaw28 <rashaunlawson28@gmail.com>
2023-01-30 17:45:38 -0500

    Add variable for backspace button and update equal event listener.

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ const equal = document.querySelector('.equal');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
 const decimal = document.querySelector('.decimal');
+const backSpace = document.querySelector('.backspace');
 
 let currentValue = '';
 let previousValue = '';
 let op = operators;
+
+
 
 numbers.forEach((number) => number.addEventListener('click', function(e){
   handleNumber(e.target.textContent);
   currentScreen.textContent = currentValue;
 }))
 
+
 operators.forEach((op) => op.addEventListener('click', function (e) {
   handleOpertator(e.target.textContent);
   previousScreen.textContent = previousValue + " " + operator;
   currentScreen.textContent = currentValue;
 }))
+
 
 function handleNumber(num) {
   if (currentValue.length <= 20) {
@@ -33,11 +38,16 @@ function handleOpertator(op) {
   currentValue = '';
 }
 
+
+
 equal.addEventListener('click', function () {
+  if (currentValue != '' && previousValue != '') {
     operate();
     previousScreen.textContent = '';
     currentScreen.textContent = previousValue;
-  });
+  }
+});
+
 
 clear.addEventListener('click', () => {
   previousValue = '';
@@ -46,6 +56,8 @@ clear.addEventListener('click', () => {
   currentScreen.textContent = currentValue;
   previousValue.textContent = currentValue;
 })
+
+
 
 decimal.addEventListener('click', function() {
   addDecimal();
@@ -56,6 +68,8 @@ function addDecimal() {
     currentValue += '.';
   }
 }
+
+
 
 const operate = function () {
   previousValue = Number(previousValue);
